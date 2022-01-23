2848cd7f7dc098f6bfc6cc457db2f30386fdbe47
Nguyễn Thành Đông <nguyenthanhdong997@gmail.com>
2022-01-23 16:21:22 +0700

    handle keydown

--- wordle/app.js
+++ wordle/app.js
@@ -3,6 +3,36 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     let grid = document.getElementById('grid')
+    let wordList = [
+        'white',
+        'space',
+        'house',
+        'piano',
+    ]
+    let randomIndex = Math.floor(Math.random() * wordList.length)
+    let secretWord = 'house'
+    
+    let attempts = []
+    let currentAttempt = ''
+    window.addEventListener('keydown',handleKeyDown)
+    function handleKeyDown(e) {
+        let letter = e.key
+        if(letter == 'Enter') {
+            //ToDo
+            if(currentAttempt.length < 5) return
+            attempts.push(currentAttempt)
+            currentAttempt = ''
+        }
+        else if (letter === 'Backspace') {
+            currentAttempt = currentAttempt.slice(0,currentAttempt.length - 1)
+        }
+        else if (/[a-z]/.test(letter)){
+            currentAttempt += letter
+        }
+        updateGrid()
+    }
+
+    buildGrid()
 
     function buildGrid() {
         for (let i = 0; i < 6; i++) {
@@ -16,20 +46,6 @@ document.addEventListener('DOMContentLoaded', () => {
             grid.appendChild(row)
         }
     }
-    buildGrid()
-    
-    let wordList = [
-        'white',
-        'space',
-        'house',
-        'piano',
-    ]
-    let randomIndex = Math.floor(Math.random() * wordList.length)
-    let secretWord = 'house'
-    
-    let attempts = []
-    let currentAttempt = 'ohude'
-    updateGrid()
 
     function updateGrid() {
         let row = grid.firstChild
@@ -39,20 +55,26 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         darawCurrentAttempt(row,currentAttempt)
     }
+    function gameCheck(attempt) {
+        if(attempt === secretWord) window.alert('ok')
+    }
+
     function darawPassAttempt(row,attempt) {
-        for (let i = 0; i < attempt.length; i++) {
+        for (let i = 0; i < 5; i++) {
             let cell = row.children[i]
             cell.textContent = attempt[i]
             cell.style.backgroundColor = getBgColor(attempt,i)
         }        
     }
+
     function darawCurrentAttempt(row,attempt) {
-        for (let i = 0; i < attempt.length; i++) {
+        for (let i = 0; i < 5; i++) {
             let cell = row.children[i]
             cell.textContent = attempt[i]
-            cell.style.backgroundColor = getBgColor(attempt,i)
+            // cell.style.backgroundColor = getBgColor(attempt,i)
         }       
     }
+
     function getBgColor(attempt,i) {
         let correctLetter = secretWord[i]
         let attemptLetter = attempt[i]
