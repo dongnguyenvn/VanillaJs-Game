458289363795631535451081243b18a664622239
Nguyễn Thành Đông <nguyenthanhdong997@gmail.com>
2022-01-24 23:50:52 +0700

    wordle

--- wordle/app.js
+++ wordle/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     let grid = document.getElementById('grid')
     let keyboard = document.getElementById('keyboard')
     let wordList = [
@@ -12,35 +12,41 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
     let randomIndex = Math.floor(Math.random() * wordList.length)
     let secretWord = 'house'
-    
-    let attempts = []
+
+    let attemptsHistory = []
     let currentAttempt = ''
+    let keyboardButton = new Map()
 
 
-    window.addEventListener('keydown',handleKeyDown)
+    window.addEventListener('keydown', handleKeyDown)
     buildGrid()
     buildKeyboard()
 
 
     function handleKeyDown(e) {
-        if(e.ctrlKey || e.metaKey || e.altKey) return
+        if (e.ctrlKey || e.metaKey || e.altKey) return
         let letter = e.key
-        if(letter == 'Enter') {
-            //ToDo
-            if(currentAttempt.length < 5) return
-            attempts.push(currentAttempt)
+        handleKey(letter)
+    }
+
+    function handleKey(letter) {
+        if(attemptsHistory.length === 6) return
+        if (letter == 'Enter') {
+            if (currentAttempt.length < 5) return
+            attemptsHistory.push(currentAttempt)
             currentAttempt = ''
+            updateKeyboard()
         }
         else if (letter === 'Backspace') {
-            currentAttempt = currentAttempt.slice(0,currentAttempt.length - 1)
+            currentAttempt = currentAttempt.slice(0, currentAttempt.length - 1)
         }
-        else if (/^[a-z]$/.test(letter)){
+        else if (/^[a-z]$/.test(letter)) {
             currentAttempt += letter
         }
         updateGrid()
     }
 
-    
+
     function buildGrid() {
         for (let i = 0; i < 6; i++) {
             let row = document.createElement('div')
@@ -53,42 +59,31 @@ document.addEventListener('DOMContentLoaded', () => {
             grid.appendChild(row)
         }
     }
-    
+
     function updateGrid() {
         let row = grid.firstChild
-        for(let attempt of attempts) {
-            darawPassAttempt(row,attempt)
+        for (let attempt of attemptsHistory) {
+            darawAttempt(row, attempt,false)
             row = row.nextSibling
         }
-        darawCurrentAttempt(row,currentAttempt)
-    }
-    function gameCheck(attempt) {
-        if(attempt === secretWord) window.alert('ok')
+        darawAttempt(row, currentAttempt,true)
     }
 
-    function darawPassAttempt(row,attempt) {
+    function darawAttempt(row, attempt,isCurrent) {
         for (let i = 0; i < 5; i++) {
             let cell = row.children[i]
             cell.textContent = attempt[i]
-            cell.style.backgroundColor = getBgColor(attempt,i)
-        }        
+            if(!isCurrent) cell.style.backgroundColor = getBgColor(attempt, i)
+        }
     }
 
-    function darawCurrentAttempt(row,attempt) {
-        for (let i = 0; i < 5; i++) {
-            let cell = row.children[i]
-            cell.textContent = attempt[i]
-            // cell.style.backgroundColor = getBgColor(attempt,i)
-        }       
-    }
-
-    function getBgColor(attempt,i) {
+    function getBgColor(attempt, i) {
         let correctLetter = secretWord[i]
         let attemptLetter = attempt[i]
-        if(secretWord.indexOf(attemptLetter) === -1) {
+        if (secretWord.indexOf(attemptLetter) === -1) {
             return '#3a3a3c'
         }
-        if(correctLetter === attemptLetter) {
+        if (correctLetter === attemptLetter) {
             return '#538d4e'
         }
         return '#b59f3b'
@@ -96,19 +91,57 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function buildKeyboard() {
         buildKeyboardRow('qwertyuiop')
-        buildKeyboardRow(['','a','s','d','f','g','h','j','k','l',''])
-        buildKeyboardRow(['enter','z','x','c','v','b','n','m','del'])
+        buildKeyboardRow(['', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ''])
+        buildKeyboardRow(['Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'del'])
     }
 
     function buildKeyboardRow(letters) {
         let row = document.createElement('div')
         row.className = 'keyboard-row'
-        for(let letter of letters) {
+        for (let letter of letters) {
             let button = document.createElement('button')
+            if (letter == '') button.className = 'spacer-half'
             button.textContent = letter
-            if(letter == '') button.className = 'spacer-half'
+            handleOnClickKeyboard(button, letter)
+            keyboardButton.set(button,letter)
             row.appendChild(button)
         }
         keyboard.appendChild(row)
+    }
+
+    function handleOnClickKeyboard(button, letter) {
+        if (letter == 'del') {
+            button.onclick = () => {
+                handleKey('Backspace')
+            }
+        } else {
+            button.onclick = () => {
+                handleKey(letter)
+            }
+        }
+    }
+
+    function updateKeyboard() {
+        let bestColors = new Map()
+        for(let attempt of attemptsHistory) {
+            for(let i = 0 ; i < attempt.length ; i++) {
+                let color = getBgColor(attempt,i)
+                let bestColor = bestColors.get(attempt[i])
+                bestColors.set(attempt[i],getBetterColor(color,bestColor))
+            }
+        }
+        for(let [button,letter] of keyboardButton) {
+            button.style.backgroundColor = bestColors.get(letter)
+        }
+    }
+
+    function getBetterColor(a,b) {
+        if(a === '#538d4e' || b === '#538d4e') {
+            return '#538d4e'
+        }
+        if(a === '#b59f3b' || b === '#b59f3b') {
+            return '#b59f3b'
+        }
+        else return '#3a3a3c'
     }
 })
